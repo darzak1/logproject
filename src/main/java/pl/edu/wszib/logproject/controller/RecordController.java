d96e40ca0c5c9b6fc2e70a72dd78992072ea62ed
darzak1 <darzak@gmail.com>
2019-05-30 15:19:20 +0200

    30.05 strona z wpisami działa

--- src/main/java/pl/edu/wszib/logproject/controller/RecordController.java
+++ src/main/java/pl/edu/wszib/logproject/controller/RecordController.java
@@ -27,3 +27,80 @@ public class RecordController {
     }
 
 }
+
+/*
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.wszib.logproject.dao.RecordDao;
+import pl.edu.wszib.logproject.domain.Log;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/logs/")
+public class RecordController {
+
+    private final RecordDao recordDao;
+
+    @Autowired
+    public RecordController(RecordDao recordDao) {
+        this.recordDao = recordDao;
+    }
+
+    @GetMapping("log")
+    public String showSignUpForm(Log log) {
+        return "dodaj-wpis";
+    }
+
+    @GetMapping("list")
+    public String showUpdateForm(Model model) {
+        model.addAttribute("logs", recordDao.findById());
+        return "index";
+    }
+
+    @PostMapping("add")
+    public String addLog(@Valid Log log, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-student";
+        }
+
+        recordDao.save(log);
+        return "redirect:list";
+    }
+
+    @GetMapping("edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Log student = recordDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Niewłaściwe Id wpisu: " + id));
+        model.addAttribute("student", student);
+        return "update-log";
+    }
+
+    @PostMapping("update/{id}")
+    public String updateStudent(@PathVariable("id") long id, @Valid Log log, BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
+            log.setId(id);
+            return "update-student";
+        }
+
+        recordDao.save(log);
+        model.addAttribute("students", recordDao.findAll());
+        return "index";
+    }
+
+    @GetMapping("delete/{id}")
+    public String deleteLog(@PathVariable("id") long id, Model model) {
+        Log log = recordDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Niewłaściwe Id wpisu: " + id));
+        recordDao.delete(log);
+        model.addAttribute("students", recordDao.findAll());
+        return "index";
+    }
+}*/
--- src/main/java/pl/edu/wszib/logproject/dao/RecordDao.java
+++ src/main/java/pl/edu/wszib/logproject/dao/RecordDao.java
@@ -3,6 +3,20 @@ package pl.edu.wszib.logproject.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.wszib.logproject.domain.Log;
 
-public interface  RecordDao extends JpaRepository<Log, Long>{
+public interface  RecordDao extends JpaRepository<Log, Long> {
 
 }
+
+/*import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.wszib.logproject.domain.Log;
+
+import java.util.List;
+
+@Repository
+public interface RecordDao extends CrudRepository<Log, Long> {
+    List<Log> findById(String name);
+
+    Object findById();
+
+}*/
